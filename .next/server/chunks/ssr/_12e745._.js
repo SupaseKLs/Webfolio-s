dc01125deb637ff491057402810ca04d78779ab2
Supasek Laobutsa <103197154+SupaseKLs@users.noreply.github.com>
2025-11-29 16:53:34 +0700

    Upload link

--- .next/server/chunks/ssr/_12e745._.js
+++ .next/server/chunks/ssr/_12e745._.js
@@ -121,8 +121,8 @@ function TimelineDemo() {
                         columnNumber: 11
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                        src: "https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/571431430_1271993438064521_2568043058392640990_n.jpg?stp=cp6_dst-jpg_s960x960_tt6&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=T1NThF8zDp0Q7kNvwF2kzDc&_nc_oc=Adlp4WVjPXDkN9HKJ23T3GOhdiA63vpEGivEvsb8ef2jO-7irxRF4XaizOgv-jKp-oUTQM16lR5oiVoki-yaEGXK&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=f2qePt4BshPTtASxnjJSJQ&oh=00_AfjjIFBnS83dapAhCXZgWLdebWbX90AiSumC4bim236s-Q&oe=6928F29B",
-                        alt: "startup template",
+                        src: "https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/571431430_1271993438064521_2568043058392640990_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEoMTkR0BP1pokAffHO3Rax8LpC6_3aQULwukLr_dpBQj6u7XJ1qtDOknbRgOTRJzLxGvX4N0FupXMHqZ00TKd0&_nc_ohc=5QHQQC8tB5UQ7kNvwFlS9t7&_nc_oc=AdnJqulPtl8x9rfjeVRQ302pvUmNJFwknoOv9RIHP-0D-H-ewITQp1ZSJHwYOGMkgc4YAn2jH8KEmxa8FaJznIyI&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=xwnLFRaWHTdR-kWQ1--Cpw&oh=00_AfglP2naEC9STXEBHFDza3Hcuwe-mGcjg81B2emoo0PMhg&oe=6930A35B",
+                        alt: "CMM KMUTT",
                         width: "100%",
                         height: "auto",
                         className: "w-full h-auto rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-96"
@@ -170,7 +170,7 @@ function TimelineDemo() {
                         className: "grid grid-cols-2 gap-4",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                             src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2gfNQoMKIBt__C58bQEGcKWuXrtkjpVY1-975N2dCblNugGfz14GJ66CuyCJiYCj_ayg&usqp=CAU",
-                            alt: "hero template",
+                            alt: "1DD",
                             width: "100%",
                             height: 500,
                             className: "w-full h-20 rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-96"
@@ -215,7 +215,7 @@ function TimelineDemo() {
                         className: "grid grid-cols-2 gap-4",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
                             src: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/gdsc-social-share.png",
-                            alt: "hero template",
+                            alt: "GDGS",
                             width: 500,
                             height: 500,
                             className: "h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
--- src/components/timeline-demo.jsx
+++ src/components/timeline-demo.jsx
@@ -16,8 +16,8 @@ export default function TimelineDemo() {
             Framer-motion Cloudflare and Google sheets.
           </p>
           <img
-            src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/571431430_1271993438064521_2568043058392640990_n.jpg?stp=cp6_dst-jpg_s960x960_tt6&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=T1NThF8zDp0Q7kNvwF2kzDc&_nc_oc=Adlp4WVjPXDkN9HKJ23T3GOhdiA63vpEGivEvsb8ef2jO-7irxRF4XaizOgv-jKp-oUTQM16lR5oiVoki-yaEGXK&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=f2qePt4BshPTtASxnjJSJQ&oh=00_AfjjIFBnS83dapAhCXZgWLdebWbX90AiSumC4bim236s-Q&oe=6928F29B"
-            alt="startup template"
+            src="https://scontent.fbkk22-6.fna.fbcdn.net/v/t39.30808-6/571431430_1271993438064521_2568043058392640990_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEoMTkR0BP1pokAffHO3Rax8LpC6_3aQULwukLr_dpBQj6u7XJ1qtDOknbRgOTRJzLxGvX4N0FupXMHqZ00TKd0&_nc_ohc=5QHQQC8tB5UQ7kNvwFlS9t7&_nc_oc=AdnJqulPtl8x9rfjeVRQ302pvUmNJFwknoOv9RIHP-0D-H-ewITQp1ZSJHwYOGMkgc4YAn2jH8KEmxa8FaJznIyI&_nc_zt=23&_nc_ht=scontent.fbkk22-6.fna&_nc_gid=xwnLFRaWHTdR-kWQ1--Cpw&oh=00_AfglP2naEC9STXEBHFDza3Hcuwe-mGcjg81B2emoo0PMhg&oe=6930A35B"
+            alt="CMM KMUTT"
             width="100%"
             height="auto"
             className="w-full h-auto rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-96"
@@ -39,7 +39,7 @@ export default function TimelineDemo() {
           <div className="grid grid-cols-2 gap-4">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2gfNQoMKIBt__C58bQEGcKWuXrtkjpVY1-975N2dCblNugGfz14GJ66CuyCJiYCj_ayg&usqp=CAU"
-              alt="hero template"
+              alt="1DD"
               width="100%"
               height={500}
               className="w-full h-20 rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-96"
@@ -59,7 +59,7 @@ export default function TimelineDemo() {
           <div className="grid grid-cols-2 gap-4">
             <img
               src="https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,q_auto:good/v1/gcs/platform-data-goog/events/gdsc-social-share.png"
-              alt="hero template"
+              alt="GDGS"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
